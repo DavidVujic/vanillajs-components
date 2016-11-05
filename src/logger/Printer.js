0230ce1a8a8ce566036b3ee50da1bbd5caa93d2f
davidvujic <david@vujic.se>
2016-11-05 20:49:01 +0100

    refactor: instance variable in stateful logger component

---
+++ src/logger/Printer.js
@@ -0,0 +1,26 @@
+function Printer(element) {
+
+    function print(message) {
+        if (typeof(message) === 'string') {
+            printText(message);
+            return;
+        }
+
+        printEvent(message);
+    }
+
+    function printText(text) {
+        element.innerHTML += '<br/>' + text;
+    }
+
+    function printEvent(e) {
+        var message = e.type + ' : ' + e.target.nodeName + ' : ' + e.target.innerHTML;
+        printText(message);
+    }
+
+    function clear() {
+        element.innerHTML = '';
+    }
+
+    return {print: print, clear: clear};
+}
