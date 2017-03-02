3efcfbfb2c801262421d4b36d10af88bb25fb3d0
davidvujic <david@vujic.se>
2017-03-02 20:59:58 +0100

    fix: formatting

--- src/list/list.js
+++ src/list/list.js
@@ -2,29 +2,29 @@ import load from 'templates';
 import * as listItem from 'listItem/listItem';
 
 export function render(props, done) {
-    load('/src/list/list.html', null, (el) => {
-        var data = props.data.slice();
+  load('/src/list/list.html', null, (el) => {
+    var data = props.data.slice();
 
-        function addListItem() {
-            var childProps = {
-                data: data.shift(),
-                onClick: props.onClick
-            };
+    function addListItem() {
+      var childProps = {
+        data: data.shift(),
+        onClick: props.onClick
+      };
 
-            listItem.render(childProps, (child) => {
-                el.querySelector('ul').appendChild(child);
-                next();
-            });
-        }
-
-        function next() {
-            if (data.length === 0) {
-                done(el);
-            } else {
-                addListItem();
-            }
-        }
-
+      listItem.render(childProps, (child) => {
+        el.querySelector('ul').appendChild(child);
         next();
-    });
+      });
+    }
+
+    function next() {
+      if (data.length === 0) {
+        done(el);
+      } else {
+        addListItem();
+      }
+    }
+
+    next();
+  });
 }
