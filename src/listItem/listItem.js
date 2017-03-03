502287c5e0fd41ae8f7fdf35524d50ef616516d0
davidvujic <david@vujic.se>
2017-03-03 14:36:21 +0100

    formatting

--- src/listItem/listItem.js
+++ src/listItem/listItem.js
@@ -1,7 +1,9 @@
 import React from 'react';
 
 function ListItem(props) {
-  return <li className='listItem' title='the listItem component' onClick={props.onClick}>{props.data}</li>;
+  return <li className='listItem'
+             title='the listItem component'
+             onClick={props.onClick}>{props.data}</li>;
 }
 
 export default ListItem;
